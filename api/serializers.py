7fe5c5feb3b997a23a598a2d990ea6c9c316e05b
Matt <mrz262@stern.nyu.edu>
2022-05-30 11:55:00 -0400

    refactor board to class based views, updated urls

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import Board, User, Task, Column
 
-class BoardSerializer(serializers.HyperlinkedModelSerializer):
-
+class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
         fields = ('id', 'username', 'password')
--- api/views.py
+++ api/views.py
@@ -1,10 +1,42 @@
-from django.shortcuts import render
+from django.http import Http404
 
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import BoardSerializer
 from .models import Board
 
-class BoardViewSet(viewsets.ModelViewSet):
-    queryset = Board.objects.all().order_by('username')
-    serializer_class = BoardSerializer
+class BoardViewSet(APIView):
+    """
+    List all boards, or create a new board.
+    """
+    def get(self, request, format=None):
+        boards = Board.objects.all()
+        serializer = BoardSerializer(boards, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BoardSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SingleBoardViewSet(APIView):
+    """
+    Get specific board
+    """
+    def get_object(self, pk):
+        try:
+            return Board.objects.get(pk=pk)
+        except Board.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        board = self.get_object(pk)
+        serializer = BoardSerializer(board)
+        return Response(serializer.data)
+
+
+    #TODO add update and delete cols/tasks
