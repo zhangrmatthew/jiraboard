8aa8658625135db4e8d3572c37b736172b48e12c
thatbrian <yes229@nyu.edu>
2022-05-29 23:07:09 -0400

    Fix references add on_delete

--- api/models.py
+++ api/models.py
@@ -3,27 +3,28 @@ from django.db import models
 
 # Create your models here.
 
-class Task(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    assignment = models.OneToOneField(User)
-    created = models.DateTimeField
-    assignment__date = models.DateTimeField
-    description = models.TextField(blank=True,null=True)
-    acceptance__criteria = models.TextField(blank=True,null=True)
-    
-class Column(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    board = models.ForeignKey(Board)
-
 class Board(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
 
-class Users(models.Model):
+class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    assignment = models.OneToOneField(User, on_delete=models.SET_DEFAULT, default=None, null=True)
+    created = models.DateTimeField
+    assignment_date = models.DateTimeField
+    description = models.TextField(blank=True,null=True)
+    acceptance_criteria = models.TextField(blank=True,null=True)
+    
+class Column(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
 
