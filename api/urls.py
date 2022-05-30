9e401b83022595052a02f46a14e1b67f87dc5983
Matt <mrz262@stern.nyu.edu>
2022-05-30 13:48:01 -0400

    added basic user post and get

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('boards/', views.BoardViewSet.as_view()),
     path('boards/<str:pk>', views.SingleBoardViewSet.as_view()),
+    path('users/', views.UserViewSet.as_view()),
+    path('users/<str:pk>', views.SingleUserViewSet.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -4,8 +4,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .serializers import BoardSerializer
-from .models import Board
+from .serializers import BoardSerializer, UserSerializer
+from .models import Board, User
 
 class BoardViewSet(APIView):
     """
@@ -40,3 +40,36 @@ class SingleBoardViewSet(APIView):
 
 
     #TODO add update and delete cols/tasks
+
+class UserViewSet(APIView):
+    """
+    List all users or create new user
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SingleUserViewSet(APIView):
+    """
+    Get specific board
+    """
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        users = self.get_object(pk)
+        serializer = UserSerializer(users)
+        return Response(serializer.data)
+
+    #TODO add linkage to boards and tasks when created some
