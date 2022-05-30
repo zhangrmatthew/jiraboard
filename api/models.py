6ca639b0bd09f4a4a22aa5d83ded2357e9819b45
Matt <mrz262@stern.nyu.edu>
2022-05-29 22:50:05 -0400

    added board and users model

--- api/models.py
+++ api/models.py
@@ -11,7 +11,19 @@ class Task(models.Model):
     assignment__date = models.DateTimeField
     description = models.TextField(blank=True,null=True)
     acceptance__criteria = models.TextField(blank=True,null=True)
+    
 class Column(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     board = models.ForeignKey(Board)
+
+class Board(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
+class Users(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+
